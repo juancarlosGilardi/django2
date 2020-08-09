d8207f584465290708ac5906cf6958df4d20a767
JuanCarlosPlus <63310685+JuanCarlosPlus@users.noreply.github.com>
2020-08-09 16:55:40 -0500

    first commit

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, Textarea
+from django.db import models
+
+
+class Students(models.Model):
+    f_name = models.CharField(max_length=100)
+    l_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Meta:
+    db_table = "students"
